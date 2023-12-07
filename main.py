92354c6277a03dba38ed046ba19851eb64e45619
brandon engstrom <bengstrom32@gmail.com>
2023-12-07 17:04:33 -0500

    another commit

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ a = FieldElement(7, 13)
 b = FieldElement(12, 13)
 c = FieldElement(6, 13)
 
-# print(a + b == c)
-# print(a == a)
+print(a + b == c)
+print(a == a)
 
 p1 = Point(-1, -1, 5, 7)
 p2 = Point(-1, 1, 5, 7)
